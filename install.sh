636439c9a4a935b5e32f0b483153b0e63f1793c5
Fullstackss <55584698+FullStack-web-dev@users.noreply.github.com>
2019-11-14 10:28:19 +0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Run each program
+xcode-select --install
+
+# Install Hushlogin
+echo ""
+echo "Install hushlogin"
+cp -f "assets/.hushlogin" $HOME/.hushlogin
+
+source config.sh
+source bin/defaults.sh
+source bin/binaries.sh
+source bin/apps.sh
+source bin/profile.sh
+source bin/custom.sh
+
+# ----------------------------------
+# APPS TO INSTALL MANUALLY
+# ----------------------------------
+# Ammps
+# Feedly
