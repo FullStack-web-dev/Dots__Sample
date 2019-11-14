f2645fa672aa1e1328b56677740f3e7f3353a1e2
Fullstackss <55584698+FullStack-web-dev@users.noreply.github.com>
2019-11-14 10:27:56 +0500

    Add files via upload

---
+++ config.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+# What binaries to install
+binaries=(
+  wget
+  libyaml
+  gsl
+  git
+  openssl
+  python
+  node
+  hub
+  zsh
+  vim --override-system-vi
+)
+
+# What apps to install
+apps=(
+  dropbox
+  google-chrome
+  qlcolorcode
+  screenflick
+  appcleaner
+  firefox
+  hazel
+  qlmarkdown
+  flash
+  sublime-text3
+  virtualbox
+  vlc
+  cloudup
+  font-m-plus
+  quicklook-json
+  skype
+  github
+  google-chrome
+  google-chrome-canary
+  kaleidoscope
+  utorrent
+  dash
+  cyberduck
+  ccleaner
+  sequel-pro
+  haroopad
+  mysqlworkbench
+  robomongo
+  filezilla
+  evernote
+)
+
+# What fonts to install
+fonts=(
+  font-m-plus
+  font-roboto
+  font-source-code-pro
+  font-open-sans
+)
+
+# What node packages to install
+npmpackages=(
+  bower
+  coffee-script
+  express
+  grunt-cli
+  jshint
+  karma-cli
+  less
+  mean-cli -g
+  mongoose
+  nodemon
+)
+
+# What Gems Installs
+geminstalls=(
+  rb-gsl
+  bootstrap-sass
+  rails
+)
